cdced9853ff37ea06554d9718f354f64315f7df3
YZarytskyi <yurazar222@gmail.com>
2022-10-17 12:36:02 +0300

    fix displaying of modals

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -7,13 +7,10 @@
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+      mobileMenu.classList.toggle('is-open');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    document.documentElement.classList.toggle('overflow-hidden');
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
--- src/js/modal.js
+++ src/js/modal.js
@@ -4,23 +4,22 @@
   let refs = {
     openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelectorAll('[data-modal-close]'),
-    backdrop: document.querySelectorAll('[data-modal-backdrop]')
+    backdrop: document.querySelectorAll('.backdrop')
   };
 
   for (let i = 0; i < refs.openModalBtn.length; i++) {
     refs.openModalBtn[i].addEventListener('click', (e) => {
       const currentDataModal = e.target.dataset.modalOpen
-      currentModal = document.querySelector(`[data-modal = ${currentDataModal}]`)
+      currentModal = document.querySelector(`[data-modal=${currentDataModal}]`)
       toggleModal()
     });
 
     refs.closeModalBtn[i] ? refs.closeModalBtn[i].addEventListener('click', toggleModal) : null;
-    refs.backdrop[i] ? refs.backdrop[i].addEventListener('click', (e) => e.target.hasAttribute('data-modal-backdrop')? toggleModal() : null) : null;
+    refs.backdrop[i] ? refs.backdrop[i].addEventListener('click', (e) => e.target.classList.contains('backdrop')? toggleModal() : null) : null;
   }
 
   function toggleModal() {
     currentModal.classList.toggle('is-hidden');
-
-    !currentModal.classList.contains('is-hidden') ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'visible';
+    document.documentElement.classList.toggle('overflow-hidden');
   }
 })();
